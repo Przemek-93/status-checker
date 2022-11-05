bba2749725fb9a2519abc48e1a47dbb292a354e1
Przemek Wasilewski <emcekwadrat47@gmail.com>
2022-11-05 14:48:00 +0100

    Add Register and Login User mechanism with entity, controller, repositories

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotificationController extends AbstractController
+{
+    #[Route('/notification', name: 'app_notification')]
+    public function index(): Response
+    {
+        return $this->render('notification/index.html.twig', [
+            'controller_name' => 'NotificationController',
+        ]);
+    }
+}
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly EntityManagerInterface $entityManager
+    ) {
+    }
+
+    #[Route('/registration', name: 'app_registration')]
+    public function index(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user
+                ->setPassword(
+                    $this->passwordHasher->hashPassword(
+                        $user,
+                        $user->getPassword()
+                    )
+                )
+                ->setRoles(['ROLE_USER']);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
